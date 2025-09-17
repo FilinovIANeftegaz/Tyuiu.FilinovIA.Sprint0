03ff883902a2c029573b300e927531669dd73686
Илья Филинов <jofaw47236@gmail.com>
2025-09-18 01:49:19 +0500

    Task1

---
+++ Tyuiu.FilinovIA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string name = "Игорь";
+int age = 45;
+Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+Console.ReadKey();
+
+namespace Tyuiu.FilinovIA.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Игорь";
+            int age = 45;
+            Console.WriteLine("Здравствуйте меня зовут "+name+" мне "+age+" лет!");
+            Console.ReadKey();
+        }
+    }
+}
+
+// код писал для вида,только сейчс понял, что в net 8.0 уже всё есть 
