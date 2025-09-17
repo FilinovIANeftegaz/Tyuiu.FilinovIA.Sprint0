2b6ceda427eca71ee6e353f95af0d39be70c05f4
Илья Филинов <jofaw47236@gmail.com>
2025-09-18 02:45:24 +0500

    Task2

---
+++ Tyuiu.FilinovIA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Tyuiu.FilinovIA.Sprint0.Task2.V0.Lib;
+
+Console.WriteLine(DataService.GetMessage("Игорь"));
+Console.ReadKey();
