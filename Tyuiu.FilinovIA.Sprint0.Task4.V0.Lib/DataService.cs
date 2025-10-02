414f43e801454bf272f21881f38c0bf7172cf392
Илья Филинов <jofaw47236@gmail.com>
2025-10-03 01:33:15 +0500

    Task4

---
+++ Tyuiu.FilinovIA.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿namespace Tyuiu.FilinovIA.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        //пример линейной структуры
+        public static int Addition(int a, int b)
+        {
+            return a + b; 
+        }
+        //пример линейной структуры
+        public static int Substraction(int a, int b)
+        {
+            return a - b;
+        }
+        //пример линейной структуры
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        //пример линейной структуры
+        public static int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Tyuiu.FilinovIA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.FilinovIA.Sprint0.Task4.V0.Lib;
+
+// See https://aka.ms/new-console-template for more information
+//пример линейной структуры прог-мы
+//вызов метода сложения addition
+Console.WriteLine(DataService.Addition(1, 5));
+//вызов метода сложения substracttion
+Console.WriteLine(DataService.Substraction(15, 5));
+//вызов метода сложения multiplication
+Console.WriteLine(DataService.Multiplication(10, 10));
+//вызов метода сложения division
+Console.WriteLine(DataService.Division(5, 5));
+
+Console.ReadKey();
