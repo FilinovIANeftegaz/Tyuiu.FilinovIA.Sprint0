99d5fdcb6df161f24920e3756bfaf0cd36801268
Илья Филинов <jofaw47236@gmail.com>
2025-10-03 00:57:20 +0500

    Task3

---
+++ Tyuiu.FilinovIA.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace Tyuiu.FilinovIA.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        //реализция метода Sum
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
